3aef7aafc3f389c25b6fb447af65b4516eee8a88
zhaoxh <1418094594@qq.com>
2023-08-02 14:25:40 +0800

    Create autopublish.sh

---
+++ .github/autopublish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+commit_name="`date +%Y%m%d%H`_commit"
+echo "====git auto push start..."
+echo $commit_name
+git add .
+git commit -m $commit_name
+npm version patch
+git push
+echo "====git auto push end..."
