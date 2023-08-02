04db9ad04fbda8842009572f39819f7bb44995fb
zhaoxh <1418094594@qq.com>
2023-08-02 15:02:23 +0800

    Delete autopublish.sh

--- .github/autopublish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-commit_name="`date +%Y%m%d%H`_commit"
-echo "====git auto push start..."
-echo $commit_name
-git add .
-git commit -m $commit_name
-npm version patch
-git push
-echo "====git auto push end..."
